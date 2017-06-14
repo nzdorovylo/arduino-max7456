af25921e1c92935065c80ad4d877236f4e659a1e
Romuald Conty <neomilium@gmail.com>
2017-06-14 15:43:55 +0200

    CharsetLoader: use static declaration

--- examples/CharsetLoader/CharsetLoader.ino
+++ examples/CharsetLoader/CharsetLoader.ino
@@ -2,14 +2,14 @@
 #include "max7456.h"
 #include <SPI.h>
 
-Max7456* osd = NULL; //Pointer representing a max7456 .
 //THIS ARRAY IS OBTAINED BY AN OTHER PROGRAM
 //Those 13.5ko of data are stored in the program memory.
 //Be awared that 0xFF are stored as 0x55 as program memory can't stand series of 0xFF (Arduino vicious problem)
 #include "charset.h"
 
-void setup() {
+Max7456 osd;
 
+void setup() {
   //Sample code : Write a complete table of character in the character memory of the max7456.
 
   charact currentChar; //represents a character as stored in memory (byte[54])
@@ -17,12 +17,12 @@ void setup() {
   Serial.begin(9600);
   SPI.begin();
   Serial.println(F("Initializing..."));
-  osd = new Max7456(10); //new Max7456 with CS on pin 9.
+  osd.init(6);
   Serial.println(F("Updating MAX7456 charset"));
   Serial.println(F("Are you sure? (y/n)"));
   bool goodChar = false;
 
-  osd->activateOSD(false); // Deactivate osd display.
+  osd.activateOSD(false); // Deactivate osd display.
   //Just some serial manipulations
   while (!goodChar) {
     while (!Serial.available())
@@ -48,7 +48,7 @@ void setup() {
     Max7456::getCARACFromProgMem(
         tableOfAllCharacters, i,
         currentChar); //Because the table is too big for ram memory
-    osd->sendCharacter(currentChar, i & 0x0f, (i & 0xf0) >> 4);
+    osd.sendCharacter(currentChar, i & 0x0f, (i & 0xf0) >> 4);
 
     for (int j = 0; j < 19; j++) //Rewind Serial.
       Serial.print(char(0x08));
